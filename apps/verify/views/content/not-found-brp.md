eb9babd41deedbf15a859efff75b6eec0a852269
Jamie Carter <jamie.carter@digital.homeoffice.gov.uk>
2024-05-02 11:33:45 +0100

    ACRS-39 Add page and supporting assets for BRP sign-in page

---
+++ apps/verify/views/content/not-found-brp.md
@@ -0,0 +1,3 @@
+The biometric residence permit (BRP) number and date of birth must match what is on our system.
+
+Check your BRP and date of birth, then try again. Or try signing in with your unique application number.
--- config.js
+++ config.js
@@ -8,5 +8,11 @@ module.exports = {
     saveAndExitTemplateId: process.env.SAVE_AND_EXIT_TEMPLATE_ID,
     submissionTemplateId: process.env.SUBMISSION_TEMPLATE_ID,
     caseworkerSubmissionTemplateId: process.env.CASEWORKER_SUBMISSION_TEMPLATE_ID
+  },
+  saveService: {
+    postgresDateFormat: 'YYYY-MM-DD HH:mm:ss',
+    port: process.env.DATASERVICE_SERVICE_PORT_HTTPS,
+    host: process.env.DATASERVICE_SERVICE_HOST &&
+      `https://${process.env.DATASERVICE_SERVICE_HOST}` || 'http://127.0.0.1'
   }
 };
