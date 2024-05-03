e32944e3bd10b4958acd1228d95116413e2de4b1
Mohamed Uwais <82108640+shamiluwais@users.noreply.github.com>
2024-05-03 17:49:52 +0100

    Create the initial skeleton page flow (#14)
    create the initial acrs/ skeleton page flow

--- apps/acrs/index.js
+++ apps/acrs/index.js
@@ -6,8 +6,64 @@ module.exports = {
   name: 'acrs',
   params: '/:action?/:id?/:edit?',
   baseUrl: '/acrs',
+  pages: {
+    '/terms-and-conditions': 'terms',
+    '/cookies': 'cookies'
+  },
   steps: {
-    '/start': {
+    '/sign-in': {
+      fields: ['sign-in-choice'],
+      forks: [{
+        target: '/sign-in-brp',
+        condition: {
+          field: 'sign-in-choice',
+          value: 'brp'
+        }
+      }],
+      next: '/sign-in-uan'
+    },
+    'sign-in-brp': {
+      fields: [],
+      next: '/sign-in-email'
+    },
+    '/sign-in-uan': {
+      fields: [],
+      next: '/sign-in-email'
+    },
+    '/sign-in-email': {
+      fields: [],
+      next: '/check-email'
+    },
+    '/check-email': {
+      fields: [],
+      next: '/select-form'
+    },
+    '/select-form': {
+      fields: [],
+      next: '/who-is-completing-form'
+    },
+    '/information-you-have-given-us': {
+      fields: [],
+      next: '/who-is-completing-form'
+    },
+    '/who-is-completing-form': {
+      fields: [],
+      next: '/full-name'
+    },
+    '/full-name': {
+      fields: [],
+      next: '/confirm'
+    },
+    '/session-expired': {
+      fields: [],
+      next: '/confirm'
+    },
+    '/link-expired': {
+      fields: [],
+      next: '/confirm'
+    },
+    '/information-saved': {
+      fields: [],
       next: '/confirm'
     },
     '/confirm': {
