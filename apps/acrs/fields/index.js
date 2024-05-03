6841044090175e017e74f66bb0ae9600ca06f775
shamiluwais <mohamed.uwais@digital.homeoffice.gov.uk>
2024-05-03 10:23:43 +0100

    fix linting error and title

--- apps/acrs/fields/index.js
+++ apps/acrs/fields/index.js
@@ -1,12 +1,12 @@
 'use strict';
 
 module.exports = {
-    'sign-in-choice': {
-        mixin: 'radio-group',
-        options: ['brp', 'uan'],
-        validate: 'required',
-        legend: {
-          className: 'visuallyhidden'
-        }
-      },
+  'sign-in-choice': {
+    mixin: 'radio-group',
+    options: ['brp', 'uan'],
+    validate: 'required',
+    legend: {
+      className: 'visuallyhidden'
+    }
+  }
 };
