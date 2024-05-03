630b772362780c6050671f9ee7dd7cac7a7dbcbc
Mohamed Uwais <82108640+shamiluwais@users.noreply.github.com>
2024-05-03 17:49:52 +0100

    Create the initial skeleton page flow (#14)
    create the initial acrs/ skeleton page flow

--- apps/acrs/fields/index.js
+++ apps/acrs/fields/index.js
@@ -1,3 +1,12 @@
 'use strict';
 
-module.exports = {};
+module.exports = {
+  'sign-in-choice': {
+    mixin: 'radio-group',
+    options: ['brp', 'uan'],
+    validate: 'required',
+    legend: {
+      className: 'visuallyhidden'
+    }
+  }
+};
