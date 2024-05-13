75f8dbbbcc26c15b3af1fddd754dc1ca6baf9d8a
Jamie Carter <jamie.carter@digital.homeoffice.gov.uk>
2024-05-13 16:21:37 +0100

    ACRS-34 Correct fields in sessionDefaults

--- config.js
+++ config.js
@@ -33,7 +33,7 @@ module.exports = {
   },
   sessionDefaults: {
     steps: ['/start', '/select-form', '/information-you-have-given-us', '/who-is-completing-form'],
-    fields: ['brp', 'uan', 'date-of-birth', 'user-email', 'login-method', 'csrf-secret', 'errorValues', 'errors']
+    fields: ['brp', 'uan', 'date-of-birth', 'user-email', 'id-type', 'csrf-secret', 'errorValues', 'errors']
   },
   hosts: {
     acceptanceTests: process.env.ACCEPTANCE_HOST_NAME || `http://localhost:${process.env.PORT || 8080}`
