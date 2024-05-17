ca020861dfca1eee337390f5a5da04cb43b552bd
TemitopeAyokuHO <138882912+TemitopeAyokuHO@users.noreply.github.com>
2024-05-17 22:53:29 +0100

    ACRS 35 Information given us
    - Added summary page to display saved fields
    - Merged changes from ACRS- 61 branch
    - Added the information you have given us html page
    - Added a partial view of the summary page in partial folder
    - Added and implemented the continue report in continue-report.js in behaviour folder

--- apps/acrs/sections/summary-data-sections.js
+++ apps/acrs/sections/summary-data-sections.js
@@ -1,7 +1,86 @@
 'use strict';
+const moment = require('moment');
+const PRETTY_DATE_FORMAT = 'DD MMMM YYYY';
 
 module.exports = {
-  start: [
-    'name'
-  ]
+  'referral-details': {
+    steps: [
+      {
+        field: 'brp',
+        omitChangeLink: true
+      },
+      {
+        field: 'uan',
+        omitChangeLink: true
+      },
+      {
+        field: 'date-of-birth',
+        parse: date => date && moment(date).format(PRETTY_DATE_FORMAT),
+        omitChangeLink: true
+      },
+      {
+        steps: '/who-completing-form',
+        field: 'who-completing-form'
+      },
+      {
+        steps: '/full-name',
+        field: 'full-name'
+      },
+      {
+        steps: '/confirm-referrer-email',
+        field: 'confirm-referrer-email',
+        parse: (list, req) => {
+          if (!req.sessionModel.get('steps').includes('/confirm-referrer-email')) {
+            return null;
+          }
+          return req.sessionModel.get('confirm-referrer-email') === 'yes' ?
+            `${req.sessionModel.get('user-email')}` :
+            `${req.sessionModel.get('referral-email')}`;
+        }
+      }
+    ]
+  },
+  'immigration-adviser-details': {
+    steps: [
+      {
+        field: 'legal-representative-fullname',
+        parse: (list, req) => {
+          if (!req.sessionModel.get('steps').includes('/immigration-adviser-details')) {
+            return null;
+          }
+          return list;
+        }
+      },
+      {
+        step: '/immigration-adviser-details',
+        field: 'legal-representative-email',
+        parse: (list, req) => {
+          if (!req.sessionModel.get('steps').includes('/immigration-adviser-details')) {
+            return null;
+          }
+          return req.sessionModel.get('is-legal-representative-email') === 'yes' ?
+            `${req.sessionModel.get('user-email')}` :
+            `${req.sessionModel.get('legal-representative-email')}`;
+        }
+      },
+      {
+        step: '/immigration-adviser-details',
+        field: 'legal-representative-phone-number'
+      },
+      {
+        step: '/immigration-adviser-details',
+        field: 'legal-representative-address',
+        parse: (list, req) => {
+          if (!req.sessionModel.get('steps').includes('/immigration-adviser-details')) {
+            return null;
+          }
+          return `${req.sessionModel.get('legal-representative-house-number')} \n` +
+            `${req.sessionModel.get('legal-representative-street')} \n` +
+            `${req.sessionModel.get('legal-representative-townOrCity')}\n` +
+            `${req.sessionModel.get('legal-representative-county')}\n` +
+            `${req.sessionModel.get('legal-representative-postcode')}`;
+        }
+      }
+    ]
+  }
 };
