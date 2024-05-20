d3838842e73501e7f4e4ff56018e615aac33d1b8
TemitopeAyokuHO <138882912+TemitopeAyokuHO@users.noreply.github.com>
2024-05-20 12:28:43 +0100

    Acrs 15 save and exit (#32)
    * ACRS 15 save and exit
    
    - Added save and exit behaviour functionality to allow user to Save and Exit form when user decides to continue later in
    save-and-exit.js file
    
    - Merged ACRS- 61 and ACRS-35 branch to display and test if they work ok
    
    - Added html pages to inform user the details has been saved

--- apps/acrs/behaviours/continue-report.js
+++ apps/acrs/behaviours/continue-report.js
@@ -53,7 +53,6 @@ module.exports = superclass => class extends superclass {
     }
     return res.redirect('/acrs/select-form');
   }
-
   // locals(req, res) {
   //   return Object.assign({}, super.locals(req, res), {
   //     visitedImagesPage: req.sessionModel.get('steps').includes('/evidence-upload')
