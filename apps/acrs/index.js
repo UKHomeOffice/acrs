871529d4c253176e8b95ecaa3b6142119b6e75eb
TemitopeAyokuHO <temitope.ayoku@digital.homeoffice.gov.uk>
2024-06-11 13:43:59 +0100

    Feedback review

--- apps/acrs/index.js
+++ apps/acrs/index.js
@@ -641,9 +641,9 @@ module.exports = {
     '/confirm': {
       behaviours: [SummaryPageBehaviour, ModifySummaryChangeLinks, Submit],
       sections: require('./sections/summary-data-sections'),
-      next: '/declaration-children'
+      next: '/declaration'
     },
-    '/declaration-children': {
+    '/declaration': {
       fields: ['children-declaration'],
       behaviours: [DeclarationBehaviour, SaveFormSession],
       locals: { showSaveAndExit: true },
