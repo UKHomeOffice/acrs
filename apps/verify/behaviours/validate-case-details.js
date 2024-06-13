fe91558c2289a3c9a049a734c76f389951902f84
Jamie Carter <jamie.carter@digital.homeoffice.gov.uk>
2024-06-13 15:07:48 +0100

    ACRS-124 Refactor validate-case-details behaviour
    Remove isValidCase from module export
    wrap Axios request in a try catch block
    Return error if Axios request errors

--- apps/verify/behaviours/validate-case-details.js
+++ apps/verify/behaviours/validate-case-details.js
@@ -5,6 +5,26 @@ const logger = require('hof/lib/logger')({ env: config.env });
 
 const baseUrl = `${config.saveService.host}:${config.saveService.port}/verify_lookup`;
 
+const isValidCase = async (req, queryColumn, queryValue) => {
+  const dob = req.form.values['date-of-birth'];
+
+  try {
+    const response = await axios.get(`${baseUrl}/${queryColumn}/${queryValue}`);
+    const validCases = response.data;
+
+    const foundCase = validCases.find(record => {
+      return record[queryColumn] === req.form.values[queryColumn] && record['date_of_birth'] === dob;
+    });
+
+    req.sessionModel.set('service', 'acrs');
+
+    return foundCase;
+  } catch (error) {
+    logger.log('error', `Could not get valid cases: ${error}`);
+    throw error;
+  }
+}
+
 module.exports = superclass => class extends superclass {
   async saveValues(req, res, next) {
     const queryColumn = req.sessionModel.get('sign-in-method') ?? null;
@@ -14,30 +34,14 @@ module.exports = superclass => class extends superclass {
       return res.redirect('/incorrect-details-brp');
     }
 
-    const validCase = await this.isValidCase(req, queryColumn, queryValue);
-    if (!validCase) {
-      return res.redirect(`/incorrect-details-${queryColumn}`);
+    try {
+      const validCase = await isValidCase(req, queryColumn, queryValue);
+      if (!validCase) {
+        return res.redirect(`/incorrect-details-${queryColumn}`);
+      }
+    } catch (error) {
+      return next(error)
     }
     return super.saveValues(req, res, next);
-  }
-
-  async isValidCase(req, queryColumn, queryValue) {
-    const dob = req.form.values['date-of-birth'];
-
-    try {
-      const response = await axios.get(`${baseUrl}/${queryColumn}/${queryValue}`);
-      const validCases = response.data;
-
-      const foundCase = validCases.find(record => {
-        return record[queryColumn] === req.form.values[queryColumn] && record['date_of_birth'] === dob;
-      });
-
-      req.sessionModel.set('service', 'acrs');
-
-      return foundCase;
-    } catch (error) {
-      logger.log('error', `Could not get valid cases: ${error}`);
-      return null;
-    }
   }
 };
