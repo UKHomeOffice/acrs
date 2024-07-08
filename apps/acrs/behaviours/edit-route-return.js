478e6c9b3ccdbe14b573535a0bb4ba61e03834b8
Michael Hart <144438643+michaelhartddat@users.noreply.github.com>
2024-07-08 17:23:43 +0100

    Refactor isContinueOnEdit for function type "condition"

--- apps/acrs/behaviours/edit-route-return.js
+++ apps/acrs/behaviours/edit-route-return.js
@@ -6,7 +6,7 @@ module.exports = superclass => class extends superclass {
         next(err);
       }
 
-      const shouldContinueOnEdit = this.isContinueOnEdit(req.form.options, req.form.values);
+      const shouldContinueOnEdit = this.isContinueOnEdit(req);
       const editReturnPath = req.sessionModel.get('edit-return-path');
       if (!shouldContinueOnEdit && editReturnPath) {
         return res.redirect(editReturnPath);
@@ -17,19 +17,24 @@ module.exports = superclass => class extends superclass {
 
   /**
    * Checks if continueOnEdit is set on the form or on the selected fork
-   *
-   * @param {Object} formOptions - The form configuration (options), typically matching index.js/steps
-   * @param {Object} formValues - The form input values, typically from the request as { field: 'value', ... }
-   * @return {boolean} true if the form should continue on edit
    */
-  isContinueOnEdit(formOptions, formValues) {
-    if (formOptions.continueOnEdit) {
+  isContinueOnEdit(req) {
+    if (req.form.options.continueOnEdit) {
       return true;
     }
-
-    const chosenOption = formOptions.forks?.find(fork => {
-      return formValues[fork.condition.field] === fork.condition.value;
+    const chosenOption = req.form.options.forks?.find(fork => {
+      return fork.condition && this.isForkSelected(fork.condition, req);
     });
     return Boolean(chosenOption && chosenOption.continueOnEdit);
+  }
+
+  /*
+   * Check if the conditions of the fork are met
+  */
+  isForkSelected(condition, req) {
+    if (typeof condition === 'function') {
+      return condition(req);
+    }
+    return req.form.values[condition.field] === condition.value;
   }
 };
--- test/_unit/edit-route-return.spec.js
+++ test/_unit/edit-route-return.spec.js
@@ -1,79 +1,190 @@
 const EditRouteReturn = require('../../apps/acrs/behaviours/edit-route-return');
 
 class SuperClass {}
-const isContinueOnEdit = (new (EditRouteReturn(SuperClass))).isContinueOnEdit;
+const objEditRouteReturn = (new (EditRouteReturn(SuperClass)));
 
 describe('isContinueOnEdit', () => {
   it('should return false if options.continueOnEdit is falsy', () => {
-    const values = { 'hof-field-A': 'some-input' };
-
-    let options = {};
-    let result = isContinueOnEdit(options, values);
+    const req = {
+      form: {
+        values: {
+          'hof-field-A': 'some-input'
+        },
+        options: {}
+      }
+    };
+    let result = objEditRouteReturn.isContinueOnEdit(req);
     expect(result).to.be.false;
 
-    options = { continueOnEdit: false };
-    result = isContinueOnEdit(options, values);
+    req.form.options.continueOnEdit = false;
+    result = objEditRouteReturn.isContinueOnEdit(req);
     expect(result).to.be.false;
   });
 
   it('should return true if options.continueOnEdit is truthy', () => {
-    const values = { 'hof-field-A': 'some-input' };
-
-    options = { continueOnEdit: true };
-    result = isContinueOnEdit(options, values);
+    const req = {
+      form: {
+        values: { 'hof-field-A': 'some-input' },
+        options: { continueOnEdit: true }
+      }
+    };
+    const result = objEditRouteReturn.isContinueOnEdit(req);
     expect(result).to.be.true;
   });
 
   it('should return true if a matching choice with continueOnEdit is found', () => {
-    const options = {
-      forks: [
-        {
-          condition: {
-            field: 'hof-field-A',
-            value: 'some-input'
-          },
-          continueOnEdit: true
+    const req = {
+      form: {
+        values: { 'hof-field-B': 'other-stuff', 'hof-field-A': 'some-input'},
+        options: {
+          forks: [
+            {
+              condition: {
+                field: 'hof-field-A',
+                value: 'some-input'
+              },
+              continueOnEdit: true
+            }
+          ]
         }
-      ]
+      }
     };
-    const values = { 'hof-field-B': 'other-stuff', 'hof-field-A': 'some-input'};
-    const result = isContinueOnEdit(options, values);
+    const result = objEditRouteReturn.isContinueOnEdit(req);
+    expect(result).to.be.true;
+  });
+
+  it('should return true if a matching choice with continueOnEdit is found whose condition is function', () => {
+    const req = {
+      form: {
+        values: { 'hof-field-B': 'other-stuff', 'hof-field-A': 'some-input'},
+        options: {
+          forks: [
+            {
+              condition: (req) => true,
+              continueOnEdit: true
+            }
+          ]
+        }
+      }
+    };
+    const result = objEditRouteReturn.isContinueOnEdit(req);
 
     expect(result).to.be.true;
   });
 
   it('should return false if no matching choice is found', () => {
-    const options = {
-      forks: [
-        {
-          condition: {
-            field: 'hof-field-A',
-            value: 'some-input'
-          },
-          continueOnEdit: true
+    const req = {
+      form: {
+        values: { 'hof-field-A': 'different-input' },
+        options: {
+          forks: [
+            {
+              condition: {
+                field: 'hof-field-A',
+                value: 'some-input'
+              },
+              continueOnEdit: true
+            }
+          ]
         }
-      ]
+      }
     };
-    const values = { 'hof-field-A': 'different-input' };
-    const result = isContinueOnEdit(options, values);
+    const result = objEditRouteReturn.isContinueOnEdit(req);
 
     expect(result).to.be.false;
   });
 
   it('should return false if a matching choice is found but continueOnEdit is falsy', () => {
-    const options = {
-      forks: [
-        {
-          condition: {
-            field: 'hof-field-A',
-            value: 'some-input'
-          }
+    const req = {
+      form: {
+        values: { 'hof-field-A': 'some-input' },
+        options: {
+          forks: [
+            {
+              condition: {
+                field: 'hof-field-A',
+                value: 'some-input'
+              },
+              continueOnEdit: false
+            }
+          ]
         }
-      ]
+      }
     };
-    const values = { 'hof-field-A': 'some-input'};
-    const result = isContinueOnEdit(options, values);
+    const result = objEditRouteReturn.isContinueOnEdit(req);
 
     expect(result).to.be.false;
   });
+});
+
+describe('isForkSelected', () => {
+  it('should return the true when condition is a function that is fulfilled', () => {
+    const condition = (req) => {
+      return req.form.values.parent === 'yes' && 
+        req.session.aggregatedValues.length === 0;
+    };
+    const req = {
+      form: {
+        values: {
+          'parent': 'yes'
+        }
+      },
+      session: {
+        aggregatedValues: []
+      }
+    };
+    const result = objEditRouteReturn.isForkSelected(condition, req);
+    expect(result).to.be.true;
+  });
+
+  it('should return false when condition is a function that is not fulfilled', () => {
+    const condition = (req) => {
+      return req.form.values.parent === 'yes' && 
+        req.session.aggregatedValues.length === 0;
+    };
+    const req = {
+      form: {
+        values: {
+          'parent': 'yes'
+        }
+      },
+      session: {
+        aggregatedValues: ['not-empty']
+      }
+    };
+    const result = objEditRouteReturn.isForkSelected(condition, req);
+    expect(result).to.be.false;
+  });
+
+  it('should return true when condition is an object field/value that is matched by values', () => {
+    const condition = {
+      field: 'hof-field-A',
+      value: 'some-input'
+    };
+    const req = {
+      form: {
+        values: {
+          'hof-field-A': 'some-input'
+        }
+      }
+    };
+    const result = objEditRouteReturn.isForkSelected(condition, req);
+    expect(result).to.be.true;
+  });
+
+  it('should return false when condition is an object field/value that is not matched by values', () => {
+    const condition = {
+      field: 'hof-field-A',
+      value: 'different-input'
+    };
+    const req = {
+      form: {
+        values: {
+          'hof-field-A': 'some-input'
+        }
+      }
+    };
+    const result = objEditRouteReturn.isForkSelected(condition, req);
+    expect(result).to.be.false;
+  });  
 });
