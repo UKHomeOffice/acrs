0bb07747c58d24344ead09f341aa9a389f6fc1a0
Michael Hart <144438643+michaelhartddat@users.noreply.github.com>
2024-07-10 17:54:10 +0100

    ACRS-185 refactor summary change link behaviour - extract remap loops into function - refactor conditionals into mappings array - add new section for UK Family

--- apps/acrs/behaviours/summary-modify-change-link.js
+++ apps/acrs/behaviours/summary-modify-change-link.js
@@ -1,46 +1,43 @@
 'use strict';
-
+// This behaviour is used to set the "Change" links on Summary pages
 const _ = require('lodash');
+
+const remapChangeLinks = (fields, mappings) => {
+  _.forEach(fields, field => {
+    const mapping = mappings.find(mapping => mapping.field === field.field);
+    if (mapping) {
+      field.changeLink = mapping.changeLink;
+    }
+  });
+  return fields;
+}
 
 module.exports = superclass => class extends superclass {
   locals(req, res) {
     const locals = super.locals(req, res);
     // set change link for looping summary fields
     if (locals.route === 'information-you-have-given-us' || locals.route === 'confirm') {
-      _.forEach(locals.rows, fields => {
-        locals.rows = locals.rows.map(row => {
-          // Add new section blocks as aggregator summaries are added
-          if (row.section === 'Family in your referral') {
-            _.forEach(fields, sectionFields => {
-              _.forEach(sectionFields, field => {
-                if (field.field === 'parent-full-name') {
-                  field.changeLink = '/acrs/parent-summary';
-                }
-                if (field.field === 'brother-or-sister-full-name') {
-                  field.changeLink = '/acrs/brother-or-sister-summary';
-                }
-                if (field.field === 'child-full-name') {
-                  field.changeLink = '/acrs/children-summary';
-                }
-                if (field.field === 'additional-family-full-name') {
-                  field.changeLink = '/acrs/additional-family-summary';
-                }
-                if (field.field === 'family-in-uk') {
-                  field.changeLink = '/acrs/family-in-uk';
-                }
-                if (field.field === 'family-member-fullname') {
-                  field.changeLink = '/acrs/family-in-uk-summary';
-                }
-                if (field.field === 'partner-full-name') {
-                  field.changeLink = '/acrs/partner-summary';
-                }
-              });
-            });
-            return row;
-          }
-          // end section block
-          return row;
-        });
+      _.forEach(locals.rows, section => {
+
+        // Fixup the "Change" link urls for the aggregate Title fields 
+        // so that the link jumps to the aggregate's summary page
+        if (section.section === 'Family in your referral') {
+          const mappings = [
+            { 'field': 'parent-full-name', 'changeLink': '/acrs/parent-summary' },
+            { 'field': 'brother-or-sister-full-name', 'changeLink': '/acrs/brother-or-sister-summary' },
+            { 'field': 'child-full-name', 'changeLink': '/acrs/children-summary' },
+            { 'field': 'additional-family-full-name', 'changeLink': '/acrs/additional-family-summary' },
+            { 'field': 'partner-full-name', 'changeLink': '/acrs/partner-summary' },
+          ];
+          section.fields = remapChangeLinks(section.fields, mappings);
+        }
+        if (section.section === 'Family that live in the UK') {
+          const mappings = [
+            { 'field': 'family-in-uk', 'changeLink': '/acrs/family-in-uk' },
+            { 'field': 'family-member-fullname', 'changeLink': '/acrs/family-in-uk-summary' },
+          ];
+          section.fields = remapChangeLinks(section.fields, mappings);
+        }
       });
     }
     return locals;
