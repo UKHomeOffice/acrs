be2ab88f79b870fc5e9d1f35d720432da2a75502
TemitopeAyokuHO <138882912+TemitopeAyokuHO@users.noreply.github.com>
2024-11-08 13:16:43 +0000

    CronJob Changes (#150)
    * CronJob Changes
    
    * Security Vulnerabilities

--- services/reports/daily_submitted_reports.js
+++ services/reports/daily_submitted_reports.js
@@ -8,13 +8,16 @@ const postgresDateFormat = config.saveService.postgresDateFormat;
 module.exports = class DailySubmittedReports {
   static async createReport(type, logger) {
     try {
-      const time10am = moment().set({h: 10, m: 0, s: 0});
+      const momentFormat = moment().utcOffset(0);
+      const time12am = momentFormat.set({h: 0, m: 0, s: 0});
+      // const time10am = moment().set({h: 10, m: 0, s: 0});
+
 
       const report = new Reports({
         type,
         tableName: 'saved_applications',
-        from: time10am.clone().subtract(1, 'day').format(postgresDateFormat),
-        to: time10am.clone().subtract(1, 'second').format(postgresDateFormat)
+        from: time12am.clone().subtract(1, 'day').format(postgresDateFormat),
+        to: time12am.clone().subtract(1, 'second').format(postgresDateFormat)
       });
 
       const response = await report.getRecordsWithProps({ timestamp: 'submitted_at' });
